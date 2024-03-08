5e967aa4cdaa5f7044a58a27bf62555092e05a18
kemboi22 <kemboielvis22@gmail.com>
2024-03-08 23:57:37 +0300

    Changes on the GLPI Docker Image

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/diouxx/glpi:latest" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/fametec/glpi:latest" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
