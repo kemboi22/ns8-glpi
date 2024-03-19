cdae1b09c44bf286a14f43319061c5277750d64c
Martin Bhuong <martin@genius.ke>
2024-03-19 09:47:36 +0100

    add test combined glpi

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/geniusdynamics/glpi:latest" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/geniusdynamics/glpi:combined" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
